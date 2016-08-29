97c2ba4c81b423c8b17500d6c3dc9f3c6968110e
Kalle Kankare <kalle.kankare@iki.fi>
2016-08-29 21:36:24 +0300

    sources: add test for SourceReader

--- lib/sources_test.go
+++ lib/sources_test.go
@@ -2,11 +2,32 @@ package thelm
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
+
+func TestSourceReader(t *testing.T) {
+	data := fmt.Sprintln("Something here")
+	in := strings.NewReader(data)
+	out := bytes.Buffer{}
+
+	sr := SourceReader{
+		Input: in,
+	}
+	sr.SetOutput(&out)
+
+	err := sr.Run()
+	require.NoError(t, err, "Run should succeed")
+
+	err = sr.Finish()
+	require.NoError(t, err, "Finish should succeed")
+
+	require.Equal(t, data, out.String(), "SourceReader should copy input to output")
+}
 
 func TestSourcefile(t *testing.T) {
 	var out bytes.Buffer
@@ -19,10 +40,10 @@ func TestSourcefile(t *testing.T) {
 
 	fp.SetOutput(&out)
 	err := fp.Run()
-	require.NoError(t, err, "Run shouldn't fail")
+	require.NoError(t, err, "Run should succeed'")
 
 	err = fp.Finish()
-	require.NoError(t, err, "Finish shouldn't fail")
+	require.NoError(t, err, "Finish should succeed'")
 
 	data, err := ioutil.ReadFile(filename)
 
