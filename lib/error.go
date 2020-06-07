0ced006e2972b977157e759752590dddd9d1c805
Kalle Kankare <kalle.kankare@iki.fi>
2020-06-07 10:04:24 +0300

    errors: Convert error handling to the standard library

--- lib/error.go
+++ lib/error.go
@@ -4,50 +4,70 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"runtime"
+	"strings"
 
-	"github.com/juju/errors"
+	"errors"
 )
 
 type ErrorHandler struct {
-	Out io.Writer
+	Out             io.Writer
 	PrintStackTrace bool
+}
+
+type ThelmError struct {
+	Err      error
+	Location string
+}
+
+func Errorf(format string, a ...interface{}) *ThelmError {
+	_, file, line, _ := runtime.Caller(2)
+	return &ThelmError{
+		Err:      fmt.Errorf(format, a...),
+		Location: fmt.Sprintf("%s:%d", file, line),
+	}
+}
+
+func (e *ThelmError) Error() string {
+	return e.Err.Error()
 }
 
 // The default error handler
 var E = ErrorHandler{
-	Out: os.Stderr,
+	Out:             os.Stderr,
 	PrintStackTrace: true,
 }
 
 // New creates a new error variable
 func (e *ErrorHandler) New(format string, a ...interface{}) error {
-	ret := errors.NewErr(format, a...)
-	ret.SetLocation(1)
-	return &ret
+	return Errorf(format, a...)
 }
 
 // Annotate increases context information to the error
 func (e *ErrorHandler) Annotate(err error, a ...interface{}) error {
-	ret := errors.Annotate(err, fmt.Sprint(a...))
-	if err, ok := ret.(*errors.Err); ok {
-		err.SetLocation(1)
-		ret = err
-	}
-	return ret
+	return Errorf("%s: %w", fmt.Sprint(a...), err)
 }
 
 // Print writes the error message to predefined io.Writer
 func (e *ErrorHandler) Print(err error, a ...interface{}) {
 	fmt.Fprint(e.Out, "Error: ")
-	if len (a) > 0 {
+	if len(a) > 0 {
 		fmt.Fprint(e.Out, a...)
 		fmt.Fprint(e.Out, ": ")
 	}
-	fmt.Fprintf(e.Out, "%s\n", err)
+	fmt.Fprintf(e.Out, "%v\n", err)
 
 	if e.PrintStackTrace {
-		fmt.Fprintln(e.Out, "Error stack:")
-		fmt.Fprintln(e.Out, errors.ErrorStack(err))
+		sb := strings.Builder{}
+		var terr *ThelmError
+		for errors.As(err, &terr) {
+			sb.WriteString(fmt.Sprintf("  %s\n", terr.Location))
+			err = terr
+		}
+		s := sb.String()
+		if s != "" {
+			fmt.Fprintf(e.Out, "Error stack:\n%s\n", s)
+		}
 	}
 }
 
